25d038e3280d14f7e55a5e453eb84fade80a7fad
Jordan Upton <jordo82@yahoo.com>
2023-06-28 15:48:11 -0400

    Update summarise_activity.R
    Added two arguments for the person & PE primary keys

--- R/summarise_activity.R
+++ R/summarise_activity.R
@@ -4,11 +4,13 @@
 #' summarise values to various possible levels.
 #'
 #' @param df The data frame with some value to be summarised
-#' @param id_map A two-column data frame mapping dim_pers_id to dim_prod_entty_id
+#' @param id_map A two-column data frame mapping person & PE primary keys
 #' @param at_level What level do we want the final results at?
 #' @param using_level What level do we want to use to summarise?
 #' @param group_vars Other than person/pe, what other grouping variables do we need?
 #' @param count_distinct Do we also want to count the distinct values of something?
+#' @param person_primary_key What is the column name of the primary key for a person?
+#' @param pe_primary_key What is the column name of the primary key for a PE?
 #'
 #' @return A data frame at the at-level & group_vars level, with the sum of a value (and maybe distinct count of some attribute)
 #' @export
@@ -38,7 +40,8 @@
 #'   filter(value > 0) %>%
 #'   summarise_activity(id_map = id_map,
 #'                      at_level = "person",
-#'                      using_level = "pe")
+#'                      using_level = "pe",
+#'                      pe_primary_key = "dim_prod_entty_id")
 #'}
 
 summarise_activity <- function(df,
@@ -50,25 +53,27 @@ summarise_activity <- function(df,
                                #Other than person/pe, what other grouping variables do we need?
                                group_vars = c("date", "metric"),
                                #do we want to count the distinct values of something?
-                               count_distinct = NULL){
+                               count_distinct = NULL,
+                               person_primary_key = "dim_pers_id",
+                               pe_primary_key = "prod_entty_dim_pers_id"){
   if(at_level == "pe" & using_level == "person"){
     stop("Can't have person-level metrics at the pe level!")
   }
 
 
-  #if we're doing anything at the pe level, then append dim_prod_entty_id
+  #if we're doing anything at the pe level, then append pe_primary_key
   if(at_level == "pe" | using_level == "pe"){
     df <- df %>%
-      dplyr::left_join(id_map, by = "dim_pers_id")
+      dplyr::left_join(id_map, by = person_primary_key)
   }
 
   #group according to the using_level
   if(using_level == "person"){
     df <- df %>%
-      dplyr::group_by_at(c("dim_pers_id", group_vars))
+      dplyr::group_by_at(c(person_primary_key, group_vars))
   }else{
     df <- df %>%
-      dplyr::group_by_at(c("dim_prod_entty_id", group_vars))
+      dplyr::group_by_at(c(pe_primary_key, group_vars))
   }
 
   #Summarise; count distinct values of a variable if provided
@@ -77,9 +82,10 @@ summarise_activity <- function(df,
       dplyr::summarise(value = sum(value)) %>%
       dplyr::ungroup()
   }else{
+    count_distinct <- rlang::enquo(count_distinct)
     df <- df %>%
       dplyr::summarise(value = sum(value),
-                count_distinct = dplyr::n_distinct(!!rlang::sym(count_distinct))) %>%
+                count_distinct = dplyr::n_distinct(!!count_distinct)) %>%
       dplyr::ungroup()
   }
 
@@ -89,17 +95,17 @@ summarise_activity <- function(df,
   #"credit" for every activity/transaction in the pe.
   if(at_level == "person" & using_level == "pe"){
     df <- df %>%
-      dplyr::inner_join(id_map, by = "dim_prod_entty_id") %>%
-      dplyr::select(-dim_prod_entty_id)
+      dplyr::inner_join(id_map, by = pe_primary_key) %>%
+      dplyr::select(-!!pe_primary_key)
   }
 
   #Rename the primary key depending on the at_level
   if(at_level == "person"){
     df <- df %>%
-      dplyr::rename(id = dim_pers_id)
+      dplyr::rename(id = !!person_primary_key)
   }else{
     df <- df %>%
-      dplyr::rename(id = dim_prod_entty_id)
+      dplyr::rename(id = !!pe_primary_key)
   }
 
   return(df)
